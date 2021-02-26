dc7f775e03c41671a1168938011d4eef2ae3fc77
iacopo.catalano <iacopomcatalano@gmail.com>
2021-02-26 13:49:03 +0100

    delete extra lines

--- Exercise 4/assets/js/robot.js
+++ Exercise 4/assets/js/robot.js
@@ -62,64 +62,6 @@ function createRobot() {
     second_arm.rotation.z = -Math.PI/3;
     meshes.push(second_arm);
 
-    const boxGeometryPH1 = new THREE.BoxGeometry(0.03, 0.155, 0.5);
-    const pliersHolder1 = new THREE.Mesh(boxGeometryPH1, grey_mesh);
-    pliersHolder1.position.set(0.05, 0.2, 3);
-    meshes.push(pliersHolder1);
-
-    const boxGeometryPH2 = new THREE.BoxGeometry(0.03, 0.155, 0.5);
-    const pliersHolder2 = new THREE.Mesh(boxGeometryPH2, grey_mesh);
-    pliersHolder2.position.set(-0.05, 0.2, 1.5);
-    meshes.push(pliersHolder2);
-
-    // Pliers grabber base 1 (PGB1)
-    const boxGeometryPGB1 = new THREE.BoxGeometry(0.08, 0.155, 0.3);
-    const pliersGrabberBase1 = new THREE.Mesh(boxGeometryPGB1, grey_mesh);
-    meshes.push(pliersGrabberBase1);
-    // Create pivot point between pliers holder 1 and pliers grabber base 1 (PH1 to PGB1)
-    /* const pivotPointPH1toPGB1 = new THREE.Object3D();
-    pliersHolder1.add(pivotPointPH1toPGB1);
-    // Set pliers rotation holder 1 (PCR) as reference for pliers grabber base 1 (PGB1)
-    pivotPointPH1toPGB1.add(pliersGrabberBase1);
-    pivotPointPH1toPGB1.position.set(0, 0, 0.18);
-    pivotPointPH1toPGB1.rotation.x -= Math.PI/6; */
-    pliersGrabberBase1.position.set(-0.05, 0, 0.15);
-    
-    // Pliers grabber base 2 (PGB1)
-    const boxGeometryPGB2 = new THREE.BoxGeometry(0.08, 0.155, 0.3);
-    const pliersGrabberBase2 = new THREE.Mesh(boxGeometryPGB2, grey_mesh);
-    meshes.push(pliersGrabberBase2);
-    // Create pivot point between pliers holder 2 and pliers grabber base 2 (PH1 to PGB2)
-    const pivotPointPH1toPGB2 = new THREE.Object3D();
-    pliersHolder1.add(pivotPointPH1toPGB2);
-    // Set pliers rotation holder 2 (PCR) as reference for pliers grabber base 2 (PGB2)
-    pivotPointPH1toPGB2.add(pliersGrabberBase2);
-    pivotPointPH1toPGB2.position.set(0, 0, -0.18);
-    pivotPointPH1toPGB2.rotation.x += Math.PI/6;
-    pliersGrabberBase2.position.set(-0.05, 0, -0.15);
-    
-    // Pliers grabber 1 (PG1)
-    const coneGeometryPG1 = new THREE.ConeGeometry(0.05, 0.5, 3);
-    const pliersGrabber1 = new THREE.Mesh(coneGeometryPG1, grey_mesh);
-    meshes.push(pliersGrabber1);
-    // Create pivot point between pliers grabber base 1 and pliers grabber 1 (PGB1 to PG1)
-    const pivotPointPGB1toPG1 = new THREE.Object3D();
-    pliersGrabberBase1.add(pivotPointPGB1toPG1);
-    // Set pliers rotation grabber base 1 (PGB1) as reference for pliers grabber 1 (PG1)
-    pivotPointPGB1toPG1.add(pliersGrabber1);
-    pliersGrabber1.position.set(0, 0.2, 0.1);
-    
-    // Pliers grabber 2 (PG2)
-    const coneGeometryPG2 = new THREE.ConeGeometry(0.05, 0.5, 3);
-    const pliersGrabber2 = new THREE.Mesh(coneGeometryPG2, grey_mesh);
-    meshes.push(pliersGrabber2);
-    // Create pivot point between pliers grabber base 2 and pliers grabber 2 (PGB1 to PG2)
-    const pivotPointPGB2toPG2 = new THREE.Object3D();
-    pliersGrabberBase2.add(pivotPointPGB2toPG2);
-    // Set pliers rotation grabber base 2 (PGB2) as reference for pliers grabber 2 (PG2)
-    pivotPointPGB2toPG2.add(pliersGrabber2);
-    pliersGrabber2.position.set(0, 0.2, -0.1);  
-
     return meshes;
 }
 
