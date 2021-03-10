f907ab44a4b1fe5dc67d48d567e7b7c3f6d6217e
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-10 21:08:36 +0100

    add groups

--- Exercise 4/assets/js/robot.js
+++ Exercise 4/assets/js/robot.js
@@ -16,11 +16,12 @@ function createRobot() {
     });
   
     // Robot primitives
+    const arm_group = new THREE.Group();
+
     const grey_base_geometry = new THREE.CylinderGeometry(2.5, 2.5, 0.8, 32);
     const gray_base = new THREE.Mesh(grey_base_geometry, grey_mesh);
     gray_base.position.set(0, 0.4, 0);
-
-    const arm_group = new THREE.Group();
+    arm_group.add(gray_base);
   
     const orange_base_geometry = new THREE.CylinderGeometry(2, 2, 0.5, 32);
     const orange_base = new THREE.Mesh(orange_base_geometry, orange_mesh);
@@ -35,53 +36,65 @@ function createRobot() {
 
     arm_group.add(base_box);
 
+    const first_junc_group = new THREE.Group();
+
     const junction_geometry = new THREE.CylinderGeometry(0.6, 0.6, 0.8, 32);
     const first_junction = new THREE.Mesh(junction_geometry, grey_mesh);
     first_junction.position.set(-0.9, 1.9, 0);
     first_junction.rotation.x = Math.PI/2
 
-    arm_group.add(first_junction);
+    first_junc_group.add(first_junction);
     
     const first_arm_geometry = new THREE.BoxGeometry(0.8, 3.5, 0.3);
     const first_arm = new THREE.Mesh(first_arm_geometry, orange_mesh);
     first_arm.position.set(-2.4, 3.4, 0.2);
     first_arm.rotation.z = Math.PI/4
 
-    arm_group.add(first_arm);
+    first_junc_group.add(first_arm);
+
+    const second_junc_group = new THREE.Group();
  
     const second_junction = new THREE.Mesh(junction_geometry, grey_mesh);
     second_junction.position.set(-3.9, 4.5, 0);
     second_junction.rotation.x = Math.PI/2
 
-    arm_group.add(second_junction);
+    second_junc_group.add(second_junction);
 
     const second_base_geometry = new THREE.CylinderGeometry(0.4, 0.6, 3, 32);
     const second_base = new THREE.Mesh(second_base_geometry, orange_mesh);
     second_base.position.set(-3.5, 4.9, -0.8);
     second_base.rotation.z = -Math.PI/3
 
-    arm_group.add(second_base);
+    second_junc_group.add(second_base);
     
     const second_arm_geometry = new THREE.CylinderGeometry(0.3, 0.3, 2, 32);
     const second_arm = new THREE.Mesh(second_arm_geometry, grey_mesh);
     second_arm.position.set(-1.5, 6.1, -0.8);
     second_arm.rotation.z = -Math.PI/3;
 
-    arm_group.add(second_arm);
+    second_junc_group.add(second_arm);
 
     const side_arm_geometry = new THREE.BoxGeometry(0.8, 2.0, 0.3);
     const arm_sides = new THREE.Mesh(side_arm_geometry, orange_mesh);
     arm_sides.position.set(-1.5, 6.1, -0.8);
     arm_sides.rotation.z = -Math.PI/3;
 
-    arm_group.add(arm_sides);
+    second_junc_group.add(arm_sides);
 
-    const hand_base_geometry = new THREE.BoxGeometry(0.8, 0.8, 0.8);
-    const hand_base = new THREE.Mesh(hand_base_geometry, orange_mesh);
-    hand_base.position.set(-0.5, 6.65, -0.8);
-    hand_base.rotation.z = -Math.PI/3;
+    const hand_group = new THREE.Group();
 
-    arm_group.add(hand_base);
+    const hand_junction_geometry = new THREE.BoxGeometry(0.8, 0.8, 0.8);
+    const hand_junction = new THREE.Mesh(hand_junction_geometry, orange_mesh);
+    hand_junction.position.set(-0.5, 6.65, -0.8);
+    hand_junction.rotation.z = -Math.PI/3;
+
+    hand_group.add(hand_junction);
+
+    second_junc_group.add(hand_group);
+
+    first_junc_group.add(second_junc_group);
+
+    arm_group.add(first_junc_group);
 
     return arm_group;
 }
