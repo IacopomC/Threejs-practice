662117a1cafc1fb67a00521c184f8692b1b0841a
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-16 21:44:27 +0100

    set new scene

---
+++ pw3/Exercise 2/assets/js/main.js
@@ -0,0 +1,65 @@
+import * as THREE from '../../../../../node_modules/three/build/three.module.js';
+import { OrbitControls } from '../../../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import cornelBox from './cornel_box.js';
+
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    // Camera
+    const fov = 75;
+    const aspect = window.innerWidth/window.innerHeight;
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 12, 12 );
+
+    // Controls
+    const controls = new OrbitControls(camera, canvas);
+
+    // Scene
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xB1ABA7);
+
+    // Light
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.PointLight(color, intensity);
+        light.position.set(0, 6, 0);
+        scene.add(light);
+    }
+
+    // Cornel Box
+    const corbelBox = cornelBox();
+    scene.add(corbelBox);
+
+    function render(time) {
+        time *= 0.001;  // convert to seconds;
+    
+        if (resizeRendererToDisplaySize(renderer)) {
+          const canvas = renderer.domElement;
+          camera.aspect = canvas.clientWidth / canvas.clientHeight;
+          camera.updateProjectionMatrix();
+        }
+        
+        renderer.render(scene, camera);
+    
+        requestAnimationFrame(render);
+      }
+      requestAnimationFrame(render);
+}
+
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
+
+main();
