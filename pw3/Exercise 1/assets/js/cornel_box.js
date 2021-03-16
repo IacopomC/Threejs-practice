a17556216a1cca65ba91ed48310082f2f2c1b09e
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-16 19:47:01 +0100

    add meshes

--- pw3/Exercise 1/assets/js/cornel_box.js
+++ pw3/Exercise 1/assets/js/cornel_box.js
@@ -2,6 +2,21 @@ import * as THREE from '../../../../../node_modules/three/build/three.module.js'
 
 function cornelBox() {
 
+    // Meshes
+    const cone_material =  new THREE.MeshLambertMaterial( {
+        color: 0xE55C08,
+        metalness: 1.5,
+        normalScale: 1.0
+    });
+
+    const cylinder_material = new THREE.MeshPhongMaterial({
+        color: 0xFF0000,
+        flatShading: true,
+      });
+
+
+    const sphere_material = new THREE.MeshPhysicalMaterial({ });
+
 }
 
 export default cornelBox;
