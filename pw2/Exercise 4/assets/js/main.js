445c606acfba731073d130ab22fad06b825d34e5
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-16 19:44:18 +0100

    fix import

--- pw2/Exercise 4/assets/js/main.js
+++ pw2/Exercise 4/assets/js/main.js
@@ -1,5 +1,5 @@
-import * as THREE from '../../../../node_modules/three/build/three.module.js';
-import { OrbitControls } from '../../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from '../../../../../node_modules/three/build/three.module.js';
+import { OrbitControls } from '../../../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import createRobot from './robot.js';
 import PickHelper from './pick_helper.js';
 import generateBalls from './ball.js';
