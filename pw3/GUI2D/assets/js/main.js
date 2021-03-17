6083410a2884c5a284567b0d6436bd3b1d0064d7
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-17 12:33:18 +0100

    add gui command cylinder, light, sphere

--- pw3/GUI2D/assets/js/main.js
+++ pw3/GUI2D/assets/js/main.js
@@ -19,36 +19,45 @@ function main() {
     // Controls
     const controls = new OrbitControls(camera, canvas);
 
-    // Gui
-    var obj1 = { speed: 5 };
-    var ui = new UIL.Gui( { css:'top:10px; left:20%;', size:300, center:true } );
-    ui.add( obj1, 'speed', { min:-5, max:5, rename:'speed R' } ).listen();
-    ui.add( obj1, 'speed', { min:-5, max:5, stype:1 } ).listen();
-    ui.add( obj1, 'speed', { min:-5, max:5, stype:2 } ).listen();
-    ui.add( obj1, 'speed', { type:'Circular', min:-5, max:5, size:120 } ).listen();
-    ui.add( obj1, 'speed', { type:'Knob', min:-5, max:5, size:120 } ).listen();
-
     // Scene
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0xB1ABA7);
 
     // Light
-    {
-        const color = 0xFFFFFF;
-        const intensity = 0.8;
-        const light = new THREE.PointLight(color, intensity);
-        light.position.set(0, 4, 0);
-        scene.add(light);
-    }
-    {
-      const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-      scene.add( light );
-    }
+    const color = 0xFFFFFF;
+    const intensity = 0.8;
+    const pointLight = new THREE.PointLight(color, intensity);
+    pointLight.position.set(0, 4, 0);
+    scene.add(pointLight);
 
+    const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add(ambientLight);
 
     // Cornel Box
     const corbelBox = cornelBox();
     scene.add(corbelBox);
+
+    let sphereCallback = function scaleSphere(value) {
+      let sphere = corbelBox.children[7];
+      let radius = sphere.geometry.parameters.radius;
+      let  scale = radius * value; // adjust the multiplier to whatever
+      sphere.scale.x = scale;
+      sphere.scale.y = scale;
+      sphere.scale.z = scale;
+    }
+
+    // Gui
+    var obj1 = { speed: 5 };
+
+    var ui = new UIL.Gui( { css:'top:10px; left:20%;', size:300, center:true } );
+    ui.add( pointLight, 'intensity', { min:0, max:5, rename:'Intensity' } ).listen();
+    ui.add( obj1, 'speed', { min:-5, max:5, stype:1 } ).listen();
+    ui.add( obj1, 'speed', { min:-5, max:5, stype:2 } ).listen();
+    ui.add( corbelBox.children[6].rotation, 'y',
+            { type:'Circular', min:-5, max:5, size:80, rename:'Cylinder Orientation' } ).listen();
+    ui.add( 'slide',
+            {name:'Sphere radius',
+             callback: sphereCallback, type:'Knob', value:1, min:0, max:2}).listen();
 
     function render(time) {
         time *= 0.001;  // convert to seconds;
