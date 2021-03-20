e0803a776c9018afbc8ea4eeb77ac43711755c7a
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-20 19:43:16 +0100

    first commit VR experimentation

---
+++ pw2/VR Experimentation/assets/js/ring.js
@@ -0,0 +1,24 @@
+import * as THREE from '../../../../../node_modules/three/build/three.module.js';
+
+function createRing() {
+
+    // Torus parameters
+    const radius = 1;  
+    const tubeRadius = 0.15;  
+    const radialSegments = 8;  
+    const tubularSegments = 24;  
+
+    const geometry = new THREE.TorusGeometry(radius, tubeRadius, radialSegments, tubularSegments);
+
+    const material = new THREE.MeshLambertMaterial( { color: 0x072904, side: THREE.DoubleSide } );
+
+    const ring = new THREE.Mesh( geometry, material );
+    
+    // Define position and orientation
+    ring.rotation.x = Math.PI/2;
+    ring.position.set(5, 5, 0);
+
+    return ring;
+}
+
+export default createRing;
