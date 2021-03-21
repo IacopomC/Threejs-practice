b6b9239b2ae2167fd1c661c124b390e82a2c4f38
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-21 16:17:44 +0100

    add rect light walls

--- pw3/Surface Lighting/assets/js/main.js
+++ pw3/Surface Lighting/assets/js/main.js
@@ -1,4 +1,5 @@
 import * as THREE from '../../../../../node_modules/three/build/three.module.js';
+import { RectAreaLightUniformsLib } from '../../../../../node_modules/three/examples/jsm/lights/RectAreaLightUniformsLib.js'
 import 'http://lo-th.github.io/uil/build/uil.js';
 import { OrbitControls } from '../../../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import cornellBox from './cornell_box.js';
@@ -36,6 +37,32 @@ function main() {
     // Cornell Box
     const cornellBoxObj = cornellBox();
     scene.add(cornellBoxObj);
+
+    // Call RectAreaLightUniformsLib
+    RectAreaLightUniformsLib.init();
+
+    // Add RectAreaLight left wall
+    const width = 6;
+    const height = 6;
+    const rectIntensity = 0.2;
+    const rectLightLeft = new THREE.RectAreaLight( 0xFF0000, rectIntensity,  width, height );
+    rectLightLeft.position.set( -3, 3, 0 );
+    rectLightLeft.rotation.set(0, Math.PI/2 , 0);
+    rectLightLeft.lookAt( 0, 0, 0 );
+    scene.add( rectLightLeft )
+
+    // Add RectAreaLight right wall
+    const rectLightRight = new THREE.RectAreaLight( 0x009D00, rectIntensity,  width, height );
+    rectLightRight.position.set( 3, 3, 0 );
+    rectLightRight.rotation.set(0, Math.PI/2 , 0);
+    rectLightRight.lookAt( 0, 0, 0 );
+    scene.add( rectLightRight )
+
+    // Add RectAreaLight back wall
+    const rectLightBack = new THREE.RectAreaLight( 0xffffff, rectIntensity,  width, height );
+    rectLightBack.position.set( 0, 3, 3 );
+    rectLightBack.lookAt( 0, 0, 0 );
+    scene.add( rectLightBack )
 
     const sphere = cornellBoxObj.children[7];
     const cylinder = cornellBoxObj.children[6];
