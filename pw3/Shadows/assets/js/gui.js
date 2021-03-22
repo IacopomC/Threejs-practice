1b56a3588c3df216358416d5cfe4ea2d13a5be6c
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-22 17:26:04 +0100

    create gui different js

---
+++ pw3/Shadows/assets/js/gui.js
@@ -0,0 +1,119 @@
+import * as THREE from '../../../../../node_modules/three/build/three.module.js';
+
+import 'https://lo-th.github.io/uil/build/uil.js';
+
+function addGui(pointLight, cornellBoxObj) {
+
+    const sphere = cornellBoxObj.children[7];
+    const cylinder = cornellBoxObj.children[6];
+    const cone = cornellBoxObj.children[5];
+
+    // Gui
+    var ui = new UIL.Gui( { css:'top:10px; left:20%;', size:300, center:true } );
+    // Light intensity
+    ui.add( pointLight, 'intensity', { min:0, max:5, rename:'Intensity' } ).listen();
+    // Wireframe
+    ui.add('bool', { name:'Wireframe', }).onChange(
+      function(value){
+        sphere.material.wireframe = value;
+        cone.material.wireframe = value;
+        cylinder.material.wireframe = value;
+      }
+    );
+    // Reflectivity
+    ui.add('slide', { min:0, max:1, value: 0.5, rename:'Reflective' }).onChange(
+      function(value){
+        sphere.material.reflectivity = value;
+        cone.material.reflectivity = value;
+        cylinder.material.reflectivity = value;
+      }
+    );
+    // Color
+    ui.add('color', { name:'Color', type:'rgba', value:[0,1,1,1]}).onChange(
+      function(color){
+        sphere.material.color.setHex(color);
+        cone.material.color.setHex(color);
+        cylinder.material.color.setHex(color);
+      }
+    );
+    // Map
+    const texture = new THREE.TextureLoader().load('./assets/img/earth.jpg');
+    ui.add('bool', { name:'Map'}).onChange(
+      function(v){
+        if (v) {
+          sphere.material.color.setHex(0xFFFFFF);
+          cone.material.color.setHex(0xFFFFFF);
+          cylinder.material.color.setHex(0xFFFFFF);
+
+          sphere.material.map = texture;
+          cone.material.map = texture;
+          cylinder.material.map = texture;
+        }
+        else{
+          sphere.material.color.setHex(0x0000FF);
+          cone.material.color.setHex(0xE55C08);
+          cylinder.material.color.setHex(0xFF0000);
+
+          sphere.material.map = null;
+          cone.material.map = null;
+          cylinder.material.map = null;
+        }
+        sphere.material.needsUpdate = true;
+        cone.material.needsUpdate = true;
+        cylinder.material.needsUpdate = true;
+      }
+    );
+
+    // Alpha Map
+    const textureChain = new THREE.TextureLoader().load('./assets/img/chainlink.png');
+    const textureChainAlpha = new THREE.TextureLoader().load('./assets/img/chainlink_alpha.png');
+    ui.add('bool', { name:'Alpha Map'}).onChange(
+      function(v){
+        if (v) {
+          sphere.material.color.setHex(0xFFFFFF);
+          cone.material.color.setHex(0xFFFFFF);
+          cylinder.material.color.setHex(0xFFFFFF);
+
+          sphere.material.map = textureChain;
+          cone.material.map = textureChain;
+          cylinder.material.map = textureChain;
+
+          sphere.material.alphaMap = textureChainAlpha;
+          cone.material.alphaMap = textureChainAlpha;
+          cylinder.material.alphaMap = textureChainAlpha;
+
+          sphere.material.transparent = true;
+          cone.material.transparent = true;
+          cylinder.material.transparent = true;
+        }
+        else{
+          sphere.material.color.setHex(0x0000FF);
+          cone.material.color.setHex(0xE55C08);
+          cylinder.material.color.setHex(0xFF0000);
+
+          sphere.material.map = null;
+          cone.material.map = null;
+          cylinder.material.map = null;
+
+          sphere.material.alphaMap = null;
+          cone.material.alphaMap = null;
+          cylinder.material.alphaMap = null;
+
+          sphere.material.transparent = false;
+          cone.material.transparent = false;
+          cylinder.material.transparent = false;
+        }
+        sphere.material.needsUpdate = true;
+        cone.material.needsUpdate = true;
+        cylinder.material.needsUpdate = true;
+      }
+    );
+
+    // Light Shadow Properties
+    ui.add( pointLight.shadow.mapSize, 'x', { min:0, max:512, value:512, rename:'Shadow X' } ).listen();
+    ui.add( pointLight.shadow.mapSize, 'y', { min:0, max:512, value:512, rename:'Shadow Y' } ).listen();
+    ui.add( pointLight.shadow, 'radius', { min:0, max:100, value:1, rename:'Shadow R' } ).listen();
+
+}
+
+export default addGui;
--- pw3/Shadows/assets/js/main.js
+++ pw3/Shadows/assets/js/main.js
@@ -3,6 +3,7 @@ import { OrbitControls } from '../../../../../node_modules/three/examples/jsm/co
 
 import cornellBox from './cornell_box.js';
 import addWallsLight from './area_light.js';
+import addGui from './gui.js';
 
 import 'http://lo-th.github.io/uil/build/uil.js';
 
@@ -36,8 +37,6 @@ function main() {
     pointLight.castShadow = true; // set light to cast shadow
     scene.add(pointLight);
 
-    console.log(pointLight.shadow);
-
     const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
     scene.add(ambientLight);
 
@@ -63,110 +62,7 @@ function main() {
     })
     
     // Gui
-    var ui = new UIL.Gui( { css:'top:10px; left:20%;', size:300, center:true } );
-    // Light intensity
-    ui.add( pointLight, 'intensity', { min:0, max:5, rename:'Intensity' } ).listen();
-    // Wireframe
-    ui.add('bool', { name:'Wireframe', }).onChange(
-      function(value){
-        sphere.material.wireframe = value;
-        cone.material.wireframe = value;
-        cylinder.material.wireframe = value;
-      }
-    );
-    // Reflectivity
-    ui.add('slide', { min:0, max:1, value: 0.5, rename:'Reflective' }).onChange(
-      function(value){
-        sphere.material.reflectivity = value;
-        cone.material.reflectivity = value;
-        cylinder.material.reflectivity = value;
-      }
-    );
-    // Color
-    ui.add('color', { name:'Color', type:'rgba', value:[0,1,1,1]}).onChange(
-      function(color){
-        sphere.material.color.setHex(color);
-        cone.material.color.setHex(color);
-        cylinder.material.color.setHex(color);
-      }
-    );
-    // Map
-    const texture = new THREE.TextureLoader().load('./assets/img/earth.jpg');
-    ui.add('bool', { name:'Map'}).onChange(
-      function(v){
-        if (v) {
-          sphere.material.color.setHex(0xFFFFFF);
-          cone.material.color.setHex(0xFFFFFF);
-          cylinder.material.color.setHex(0xFFFFFF);
-
-          sphere.material.map = texture;
-          cone.material.map = texture;
-          cylinder.material.map = texture;
-        }
-        else{
-          sphere.material.color.setHex(0x0000FF);
-          cone.material.color.setHex(0xE55C08);
-          cylinder.material.color.setHex(0xFF0000);
-
-          sphere.material.map = null;
-          cone.material.map = null;
-          cylinder.material.map = null;
-        }
-        sphere.material.needsUpdate = true;
-        cone.material.needsUpdate = true;
-        cylinder.material.needsUpdate = true;
-      }
-    );
-
-    // Alpha Map
-    const textureChain = new THREE.TextureLoader().load('./assets/img/chainlink.png');
-    const textureChainAlpha = new THREE.TextureLoader().load('./assets/img/chainlink_alpha.png');
-    ui.add('bool', { name:'Alpha Map'}).onChange(
-      function(v){
-        if (v) {
-          sphere.material.color.setHex(0xFFFFFF);
-          cone.material.color.setHex(0xFFFFFF);
-          cylinder.material.color.setHex(0xFFFFFF);
-
-          sphere.material.map = textureChain;
-          cone.material.map = textureChain;
-          cylinder.material.map = textureChain;
-
-          sphere.material.alphaMap = textureChainAlpha;
-          cone.material.alphaMap = textureChainAlpha;
-          cylinder.material.alphaMap = textureChainAlpha;
-
-          sphere.material.transparent = true;
-          cone.material.transparent = true;
-          cylinder.material.transparent = true;
-        }
-        else{
-          sphere.material.color.setHex(0x0000FF);
-          cone.material.color.setHex(0xE55C08);
-          cylinder.material.color.setHex(0xFF0000);
-
-          sphere.material.map = null;
-          cone.material.map = null;
-          cylinder.material.map = null;
-
-          sphere.material.alphaMap = null;
-          cone.material.alphaMap = null;
-          cylinder.material.alphaMap = null;
-
-          sphere.material.transparent = false;
-          cone.material.transparent = false;
-          cylinder.material.transparent = false;
-        }
-        sphere.material.needsUpdate = true;
-        cone.material.needsUpdate = true;
-        cylinder.material.needsUpdate = true;
-      }
-    );
-
-    // Light Shadow Properties
-    ui.add( pointLight.shadow.mapSize, 'x', { min:0, max:512, value:512, rename:'Shadow X' } ).listen();
-    ui.add( pointLight.shadow.mapSize, 'y', { min:0, max:512, value:512, rename:'Shadow Y' } ).listen();
-    ui.add( pointLight.shadow, 'radius', { min:0, max:100, value:1, rename:'Shadow R' } ).listen();
+    addGui(pointLight, cornellBoxObj);
 
     function render() {
     
