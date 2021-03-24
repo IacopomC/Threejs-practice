1514e4320100fa0ff9cbc337926f6ce0ff770d55
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-24 20:03:12 +0100

    implement play/pause functionality buttons

--- sprint/assets/js/main.js
+++ sprint/assets/js/main.js
@@ -1,6 +1,5 @@
 import * as THREE from '../../../../../node_modules/three/build/three.module.js';
 import { OrbitControls } from '../../../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
-import {GUI} from "../../../../../node_modules/three/examples/jsm/libs/dat.gui.module.js";
 import {VRButton} from '../../../../../node_modules/three/examples/jsm/webxr/VRButton.js';
 import { BoxLineGeometry } from '../../../../../node_modules/three/examples/jsm/geometries/BoxLineGeometry.js';
 
@@ -51,8 +50,6 @@ var video, videoTexture;
 
 var imageProcessing, imageProcessingMaterial;
 
-// GUI
-var gui;
 
 init();
 
@@ -147,21 +144,27 @@ function init() {
     // Add console buttons
     {
       const radius = 0.1;
-      addSolidGeometry(0.65, 2, 0, Math.PI/4, 0, -Math.PI/4, new THREE.TetrahedronGeometry(radius), 0xff0000, scene, buttons);
+      addSolidGeometry(0.65, 2, 0, Math.PI/4, 0, -Math.PI/4,
+        new THREE.TetrahedronGeometry(radius), 0xff0000, scene,
+        buttons, 'play');
     }
 
     {
       const width = 0.1;
       const height = 0.1;
       const depth = 0.05;
-      addSolidGeometry(0.65, 2.2, 0, 0, 0, 0, new THREE.BoxGeometry(width, height, depth), 0x00ff00, scene, buttons);
+      addSolidGeometry(0.65, 2.2, 0, 0, 0, 0,
+        new THREE.BoxGeometry(width, height, depth), 0x00ff00, scene,
+        buttons, 'stop');
     }
 
     {
       const radius = 0.07;
       const widthSegments = 12;
       const heightSegments = 8;
-      addSolidGeometry(0.65, 1.8, 0, 0, 0, 0, new THREE.SphereGeometry(radius, widthSegments, heightSegments), 0x0000ff, scene, buttons);
+      addSolidGeometry(0.65, 1.8, 0, 0, 0, 0,
+        new THREE.SphereGeometry(radius, widthSegments, heightSegments), 0x0000ff, scene,
+        buttons, 'addSecs');
     }
 
     // Elevation Map
@@ -228,27 +231,6 @@ function init() {
     points.scale.set(0.5, 0.5, 0.5);
     scene.add(points);
  */
-    var pausePlayObj =
-    {
-      pausePlay: function () {
-        if (!video.paused) {
-          console.log("pause");
-          video.pause();
-        }
-        else {
-          console.log("play");
-          video.play();
-        }
-      },
-      add10sec: function () {
-        video.currentTime = video.currentTime + 10;
-        console.log(video.currentTime);
-      }
-    };
-
-    gui = new GUI();
-    gui.add(pausePlayObj, 'pausePlay').name('Pause/play video');
-    gui.add(pausePlayObj, 'add10sec').name('Add 10 seconds');
 
     //video.play();
 
@@ -299,8 +281,22 @@ function init() {
     pickPosition.y = -100000;
   }
 
-  function clickButton(event){
-    console.log(event);
+  function clickButton(){
+    if(pickHelper.pickedObject) {
+      switch(pickHelper.pickedObject.name){
+        case 'stop':
+          video.pause();
+          break;
+        case 'play':
+          video.play();
+          break;
+        case 'addSecs':
+          video.currentTime = video.currentTime + 10;
+          break;
+        default:
+          console.log('Select action');
+      }
+    }
   }
 
   function render() {
@@ -332,19 +328,20 @@ function onWindowResize() {
   render();
 }
 
-function addSolidGeometry(x, y, z, thetax, thetay, thetaz, geometry, color, scene, buttons) {
+function addSolidGeometry(x, y, z, thetax, thetay, thetaz, geometry, color, scene, buttons, name) {
   const material = new THREE.MeshPhongMaterial({
     side: THREE.DoubleSide,
     color: color
   });
   const mesh = new THREE.Mesh(geometry, material);
-  addObject(x, y, z, thetax, thetay, thetaz, mesh, scene, buttons);
+  addObject(x, y, z, thetax, thetay, thetaz, mesh, scene, buttons, name);
 }
 
-function addObject(x, y, z, thetax, thetay, thetaz, obj, scene, buttons) {
+function addObject(x, y, z, thetax, thetay, thetaz, obj, scene, buttons, name) {
   obj.position.x = x;
   obj.position.y = y;
   obj.position.z = z;
+  obj.name = name;
 
   obj.receiveShadow = true;
   obj.castShadow = true;
