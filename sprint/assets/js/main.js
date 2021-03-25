cb85bb451eff4b9780980f4844779bb0acd00b4b
U-EMPTRECIEREC006\ixrplatform <iacopomcatalano@gmail.com>
2021-03-25 11:45:26 +0100

    put console button different js

--- sprint/assets/js/main.js
+++ sprint/assets/js/main.js
@@ -5,6 +5,7 @@ import { BoxLineGeometry } from '../../../../../node_modules/three/examples/jsm/
 
 import ControllerPickHelper from './controller_pick_helper.js';
 import PickHelper from './pick_helper.js';
+import createConsole from './console_buttons.js';
 
 import {vertexShader, fragmentShader,
         elevationVertexShader, elevationFragmentShader,
@@ -43,7 +44,7 @@ function IVprocess(imageProcessing, renderer) {
 
 let camera, controls, scene, renderer, container, controllerPickHelper;
 let plane;
-const buttons = [];
+let buttons = [];
 
 // VIDEO AND THE ASSOCIATED TEXTURE
 var video, videoTexture;
@@ -141,31 +142,7 @@ function init() {
     plane.castShadow = true;
     scene.add(plane);
 
-    // Add console buttons
-    {
-      const radius = 0.1;
-      addSolidGeometry(0.65, 2, 0, Math.PI/4, 0, -Math.PI/4,
-        new THREE.TetrahedronGeometry(radius), 0xff0000, scene,
-        buttons, 'play');
-    }
-
-    {
-      const width = 0.1;
-      const height = 0.1;
-      const depth = 0.05;
-      addSolidGeometry(0.65, 2.2, 0, 0, 0, 0,
-        new THREE.BoxGeometry(width, height, depth), 0x00ff00, scene,
-        buttons, 'stop');
-    }
-
-    {
-      const radius = 0.07;
-      const widthSegments = 12;
-      const heightSegments = 8;
-      addSolidGeometry(0.65, 1.8, 0, 0, 0, 0,
-        new THREE.SphereGeometry(radius, widthSegments, heightSegments), 0x0000ff, scene,
-        buttons, 'addSecs');
-    }
+    buttons = createConsole(scene);
 
     // Elevation Map
     const lightDir = new THREE.Vector3 (-.5,-.5,.9);
@@ -202,7 +179,7 @@ function init() {
     scene.add(plane);
 
     // Color Cloud
-    const colClDiscret = 1;
+    /* const colClDiscret = 1;
 
     var colorSpaceMaterial = new THREE.ShaderMaterial({
       vertexShader: colorCloudVertexShader,
@@ -232,7 +209,7 @@ function init() {
     const points = new THREE.Points(geometry, colorSpaceMaterial);
     points.position.set(-0.8, 1.8, 0.3);
     points.scale.set(0.5, 0.5, 0.5);
-    scene.add(points);
+    scene.add(points); */
  
     //video.play();
 
@@ -330,26 +307,3 @@ function onWindowResize() {
   render();
 }
 
-function addSolidGeometry(x, y, z, thetax, thetay, thetaz, geometry, color, scene, buttons, name) {
-  const material = new THREE.MeshPhongMaterial({
-    side: THREE.DoubleSide,
-    color: color
-  });
-  const mesh = new THREE.Mesh(geometry, material);
-  addObject(x, y, z, thetax, thetay, thetaz, mesh, scene, buttons, name);
-}
-
-function addObject(x, y, z, thetax, thetay, thetaz, obj, scene, buttons, name) {
-  obj.position.x = x;
-  obj.position.y = y;
-  obj.position.z = z;
-  obj.name = name;
-
-  obj.receiveShadow = true;
-  obj.castShadow = true;
-
-  obj.rotation.set(thetax, thetay, thetaz);
-
-  scene.add(obj);
-  buttons.push(obj)
-}
