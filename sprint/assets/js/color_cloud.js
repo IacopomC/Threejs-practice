e924251cde0fa9b9cb73ada009c5a51ed2cbbe8b
U-EMPTRECIEREC006\ixrplatform <iacopomcatalano@gmail.com>
2021-03-25 13:11:11 +0100

    move color cloud different js

---
+++ sprint/assets/js/color_cloud.js
@@ -0,0 +1,38 @@
+import {colorCloudVertexShader, colorCloudFragmentShader} from "./shaders.js";
+import * as THREE from '../../../../../node_modules/three/build/three.module.js';
+
+function colorCloud(scene, video, videoTexture) {
+    const colClDiscret = 1;
+
+    var colorSpaceMaterial = new THREE.ShaderMaterial({
+      vertexShader: colorCloudVertexShader,
+      fragmentShader: colorCloudFragmentShader,
+      uniforms: {
+        tex: { value: videoTexture },
+        colorSpace: {value: 0}
+      }
+    });
+
+    const geometry = new THREE.BufferGeometry();
+    const positions = [];
+    for (let i = 0; i < video.videoHeight; i += colClDiscret) {
+      for (let j = 0; j < video.videoWidth; j += colClDiscret) {
+        // positions
+
+        const x = (i+0.5) / video.videoHeight;
+        const y = (j+0.5) / video.videoWidth;
+        const z = 0;
+
+        positions.push(x, y, z);
+      }
+    }
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    geometry.computeBoundingSphere();
+
+    const points = new THREE.Points(geometry, colorSpaceMaterial);
+    points.position.set(-0.8, 1.8, 0.3);
+    points.scale.set(0.5, 0.5, 0.5);
+    scene.add(points); 
+}
+
+export default colorCloud;
--- sprint/assets/js/main.js
+++ sprint/assets/js/main.js
@@ -7,10 +7,9 @@ import ControllerPickHelper from './controller_pick_helper.js';
 import PickHelper from './pick_helper.js';
 import createConsole from './console_buttons.js';
 import elevationMap from './elevationMap.js';
+import colorCloud from './color_cloud.js';
 
-import {vertexShader, fragmentShader,
-        elevationVertexShader, elevationFragmentShader,
-        colorCloudVertexShader, colorCloudFragmentShader} from "./shaders.js";
+import {vertexShader, fragmentShader} from "./shaders.js";
 
 function IVimageProcessing(height, width, imageProcessingMaterial) {
   
@@ -145,40 +144,11 @@ function init() {
 
     buttons = createConsole(scene);
 
+    // Elevation Map
     elevationMap(scene, video, videoTexture);
 
     // Color Cloud
-    /* const colClDiscret = 1;
-
-    var colorSpaceMaterial = new THREE.ShaderMaterial({
-      vertexShader: colorCloudVertexShader,
-      fragmentShader: colorCloudFragmentShader,
-      uniforms: {
-        tex: { value: videoTexture },
-        colorSpace: {value: 0}
-      }
-    });
-
-    geometry = new THREE.BufferGeometry();
-    const positions = [];
-    for (let i = 0; i < video.videoHeight; i += colClDiscret) {
-      for (let j = 0; j < video.videoWidth; j += colClDiscret) {
-        // positions
-
-        const x = (i+0.5) / video.videoHeight;
-        const y = (j+0.5) / video.videoWidth;
-        const z = 0;
-
-        positions.push(x, y, z);
-      }
-    }
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-    geometry.computeBoundingSphere();
-
-    const points = new THREE.Points(geometry, colorSpaceMaterial);
-    points.position.set(-0.8, 1.8, 0.3);
-    points.scale.set(0.5, 0.5, 0.5);
-    scene.add(points); */
+    colorCloud(scene, video, videoTexture);
  
     //video.play();
 
